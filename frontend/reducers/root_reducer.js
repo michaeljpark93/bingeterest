1e568ed3af995e7a4c7a56a2a733873741608302
Michael <michaeljpark93@gmail.com>
2018-08-07 10:59:56 -0700

    Create users reducer and initialize entities reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 
+import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 
-
 export default combineReducers({
+  entities: entitiesReducer,
   session: sessionReducer
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
 
 const _nullUser = Object.freeze({ id: null });
 
