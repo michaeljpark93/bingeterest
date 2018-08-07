0de762d94ea757c6165d13c924f13d584e67e195
Michael <michaeljpark93@gmail.com>
2018-08-07 11:08:45 -0700

    Create session errors reducer and intialize errors reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import entitiesReducer from './entities_reducer';
+import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
 
 export default combineReducers({
   entities: entitiesReducer,
+  errors: errorsReducer,
   session: sessionReducer
 });
