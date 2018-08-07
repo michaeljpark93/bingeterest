2adc173b3bc698e68236f7e5d694bf57241b3272
Michael <michaeljpark93@gmail.com>
2018-08-06 22:30:02 -0700

    Create Api Users jbuilder view and util/session_api_util requests

--- frontend/bingeterest.jsx
+++ frontend/bingeterest.jsx
@@ -5,6 +5,7 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root')
   const store = configureStore();
-  ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+  ReactDOM.render(<Root store={store}/>, root);
 })
