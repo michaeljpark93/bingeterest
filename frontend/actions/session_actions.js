eb67317803bdb060389f79fa1ef2256a418cba60
Michael <michaeljpark93@gmail.com>
2018-08-06 22:54:07 -0700

    Configure session actions and reducer and combined into root reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,41 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = formUser => dispatch => (
+  SessionAPIUtil.signup(formUser).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = formUser => dispatch => (
+  SessionAPIUtil.login(formUser).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout().then(user => (
+    dispatch(logoutCurrentUser())
+  ))
+);
