b7a8daec095ec8537f6108351fe6eae71c3211a2
Michael <michaeljpark93@gmail.com>
2018-08-08 19:08:30 -0700

    Add demo User in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demo_user = User.create(
+  id: 1,
+  username: 'Guest',
+  description: 'Demo login user',
+  password: 'password',
+  age: 100
+)
