1bc19b14efc6987a2b00239373e1de03b61a8b0f
Michael <michaeljpark93@gmail.com>
2018-09-25 00:12:05 -0700

    Refactor binge index to render more images as user scrolls

--- frontend/components/dashboard/binge_index.jsx
+++ frontend/components/dashboard/binge_index.jsx
@@ -4,27 +4,78 @@ import BingeItemShow from '../binge/binge_item_show.jsx';
 import NavBarContainer from '../navbar/nav_bar_container';
 
 class BingeIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasMore: true,
+      isLoading: false,
+      binges: null,
+      renderBinges: null,
+    };
+    const background = document.getElementsByClassName('discover-box')[0];
+    this.onScroll = this.onScroll.bind(this);
+    this.addBinges = this.addBinges.bind(this);
+  }
+
   componentDidMount() {
     const { fetchBinges, fetchBoards } = this.props;
-    fetchBinges();
+    fetchBinges().then((bingeData) => {
+      const binges = Object.values(bingeData.binges);
+      const renderBinges = binges.slice(0, 15);
+      this.setState({ binges, renderBinges });
+    });
     fetchBoards();
+    window.addEventListener('scroll', this.onScroll, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll() {
+    const { binges, renderBinges, hasMore } = this.state;
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 200)
+      && binges.length) {
+      if (hasMore) {
+        this.addBinges();
+      }
+    }
+  }
+
+  addBinges() {
+    const { binges, renderBinges } = this.state;
+    const { length } = binges;
+    const idx = renderBinges.length;
+    if (renderBinges.length >= length) {
+      this.setState({ hasMore: false });
+    } else {
+      const newBinges = binges.slice(idx, idx + 15);
+      this.setState({
+        renderBinges: [...renderBinges, ...newBinges],
+      });
+    }
   }
 
   render() {
-    const { binges, boards } = this.props;
-    return (
-      <div>
-        <NavBarContainer />
+    const { boards } = this.props;
+    const { renderBinges } = this.state;
 
-        <div className="discover">
-          <div className="discover-box">
-            <ul className="masonry">
-              {binges.map(binge => <BingeItemShow binge={binge} key={binge.id} boards={boards} />)}
-            </ul>
+    if (renderBinges !== null) {
+      return (
+        <div>
+          <NavBarContainer />
+
+          <div className="discover">
+            <div className="discover-box">
+              <ul className="masonry">
+                {renderBinges.map(binge => <BingeItemShow binge={binge} key={binge.id} boards={boards} />)}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    return null;
   }
 }
 
