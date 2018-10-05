ab214e046be2e43ca41ba36acd6d8908a4f2525e
Michael <michaeljpark93@gmail.com>
2018-10-05 15:36:32 -0700

    Add search reducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -6,6 +6,7 @@ import boardsReducer from './boards_reducer';
 import bingingsReducer from './bingings_reducer';
 import followingReducer from './following_reducer';
 import followedReducer from './followed_reducer';
+import searchReducer from './search_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
@@ -14,6 +15,7 @@ const entitiesReducer = combineReducers({
   bingings: bingingsReducer,
   following: followingReducer,
   followed: followedReducer,
+  search: searchReducer,
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/entities/search_reducer.js
@@ -0,0 +1,17 @@
+import merge from 'lodash/merge';
+import { RECEIVE_SEARCH_RESULTS, RESET_SEARCH_RESULTS } from '../../actions/search_actions';
+
+const searchReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_SEARCH_RESULTS:
+      return merge({}, action.searchResults);
+    case RESET_SEARCH_RESULTS:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default searchReducer;
